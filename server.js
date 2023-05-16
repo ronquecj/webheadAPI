2b0b2199ea3c36a21d6800be972a1229c4723618
christian <ronquechristian@gmail.com>
2023-05-16 11:56:15 +0800

    asd

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './backend/config.env' });
+dotenv.config({ path: './config.env' });
 const app = require('./backend/app');
 
 const DB = process.env.DATABASE.replace(
